a2e2b24dd023eb596b0b7c0866c03b2c45597bb3
Aurélien Haie <>
2021-03-01 23:19:46 +0100

    Setup the project

---
+++ iot_app/iot_app/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  iot_app
+//
+//  Created by Aurélien Haie on 01/03/2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    // MARK: Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
