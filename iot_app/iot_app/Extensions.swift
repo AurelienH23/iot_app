30f0cae44dccbc56b42eb6a3314682e50d37500a
Aurélien Haie <>
2021-03-02 16:31:28 +0100

    [FEAT] Started working on the Home, and initialized some extensions

---
+++ iot_app/iot_app/Extensions.swift
@@ -0,0 +1,124 @@
+//
+//  Extensions.swift
+//  iot_app
+//
+//  Created by Aurélien Haie on 02/03/2021.
+//
+
+import UIKit
+
+extension UIView {
+
+    func addSubviews(_ views: UIView...) {
+        views.forEach { (view) in
+            addSubview(view)
+        }
+    }
+    
+    func anchor(top: NSLayoutYAxisAnchor? = nil,
+                left: NSLayoutXAxisAnchor? = nil,
+                bottom: NSLayoutYAxisAnchor? = nil,
+                right: NSLayoutXAxisAnchor? = nil,
+                paddingTop: CGFloat = 0,
+                paddingLeft: CGFloat = 0,
+                paddingBottom: CGFloat = 0,
+                paddingRight: CGFloat = 0,
+                width: CGFloat = 0,
+                height: CGFloat = 0) {
+
+        translatesAutoresizingMaskIntoConstraints = false
+
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        if let left = left {
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        if let bottom = bottom {
+            self.bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        if let right = right {
+            self.rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        if width != 0 {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        if height != 0 {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+
+    func anchor(to view: UIView) {
+        anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+    }
+
+    func anchor(to view: UIView, padding: CGFloat) {
+        anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: padding, paddingLeft: padding, paddingBottom: padding, paddingRight: padding, width: 0, height: 0)
+    }
+
+    func centerVertically(to view: UIView) {
+        centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+
+    func centerHorizontally(to view: UIView) {
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+
+}
+
+extension UIColor {
+
+    static let backgroundColor = UIColor(named: "backgroundColor")
+    static let elementBackground = UIColor(named: "elementBackground")
+
+}
+
+extension CGFloat {
+
+    static var topPadding: CGFloat {
+        return UIApplication.shared.statusBarFrame.height
+    }
+
+    static var bottomPadding: CGFloat {
+        var bottomPadding: CGFloat = 0
+        if self.topPadding > 20 {
+            bottomPadding = 20
+        }
+        return bottomPadding
+    }
+
+    static var tabBarHeight: CGFloat {
+        var bottomPadding: CGFloat = 50
+        if self.topPadding > 20 {
+            bottomPadding = 63
+        }
+        return bottomPadding
+    }
+
+    // Corner radius
+    /** 4 */
+    static let smallCornerRadius = CGFloat(integerLiteral: 4)
+    /** 8 */
+    static let mediumCornerRadius = CGFloat(integerLiteral: 8)
+    /** 16 */
+    static let largeCornerRadius = CGFloat(integerLiteral: 16)
+    /** 32 */
+    static let extraLargeCornerRadius = CGFloat(integerLiteral: 32)
+
+    // Spacing
+    /** 8 */
+    static let smallSpace = CGFloat(integerLiteral: 8)
+    /** 16 */
+    static let mediumSpace = CGFloat(integerLiteral: 16)
+    /** 24 */
+    static let largeSpace = CGFloat(integerLiteral: 24)
+    /** 32 */
+    static let extraLargeSpace = CGFloat(integerLiteral: 32)
+    /** 44 */
+    static let standardTouchSpace = CGFloat(integerLiteral: 44)
+
+    func showPercent() -> String {
+        return "\(Int(self * 100))"
+    }
+
+}
--- iot_app/iot_app/HomeViewController.swift
+++ iot_app/iot_app/HomeViewController.swift
@@ -9,10 +9,86 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    // MARK: View elements
+
+    private let profileButton = RoundedButton()
+    let titleLabel = WelcomeTitle(name: "TEST")
+
     // MARK: Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+    }
+
+    private func setupViews() {
+        view.backgroundColor = .backgroundColor
+        view.addSubviews(profileButton, titleLabel)
+        profileButton.anchor(top: view.topAnchor, right: view.rightAnchor, paddingTop: .topPadding + .mediumSpace, paddingRight: .extraLargeSpace)
+        titleLabel.anchor(top: view.topAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 100, paddingLeft: .extraLargeSpace, paddingRight: .extraLargeSpace)
+    }
+
+}
+
+class WelcomeTitle: UILabel {
+
+    // MARK: Properties
+
+    private var username: String
+
+    // MARK: Lifecycle
+
+    init(name: String) {
+        username = name
+        super.init(frame: .zero)
+        setupViews()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Custom funcs
+
+    private func setupViews() {
+        numberOfLines = 2
+        let generatedText = NSMutableAttributedString(attributedString: NSAttributedString(string: "Welcome home,\n", attributes: [.font: UIFont.boldSystemFont(ofSize: 30)]))
+        generatedText.append(NSAttributedString(string: "Arthur", attributes: [.font: UIFont.systemFont(ofSize: 30)]))
+        attributedText = generatedText
+    }
+
+}
+
+class RoundedButton: UIButton {
+    
+    // MARK: Properties
+    
+    // MARK: View elements
+    
+    // MARK: Lifecycle
+    
+    init(image: String) {
+        super.init(frame: .zero)
+        setupViews()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Custom funcs
+    
+    private func setupViews() {
+        anchor(width: .standardTouchSpace, height: .standardTouchSpace)
+        layer.cornerRadius = .largeCornerRadius
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.white.cgColor
+        backgroundColor = .elementBackground
     }
 
 }
